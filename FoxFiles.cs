5c9bd4e57e1c2f77266b27e8b5688b1c42b65cb5
Fox <dev.fooxiie@gmail.com>
2024-01-08 23:00:09 +0100

    Initial commit

---
+++ FoxFiles.cs
@@ -0,0 +1,15 @@
+﻿using Life;
+
+namespace FoxFiles
+{
+    public class FoxFiles: Plugin
+    {
+        public FoxFiles(IGameAPI api) : base(api)
+        {
+            UIManager.GetInstance().SpawnMenu();
+            UIManager.GetInstance().SpawnAdminMenu();
+        }
+        
+        
+    }
+}
---
+++ UIManager.cs
@@ -0,0 +1,32 @@
+﻿using MyMenu.Entities;
+
+namespace FoxFiles
+{
+    public class UIManager
+    {
+        private static UIManager _instance = null;
+
+        private UIManager()
+        {
+        }
+
+        public static UIManager GetInstance()
+        {
+            return _instance ?? (_instance = new UIManager());
+        }
+
+        public void SpawnMenu()
+        {
+            Section section = new Section(Section.GetSourceName(), Section.GetSourceName(), "v1.0.0", "Fooxiie");
+            section.Insert();
+        }
+
+        public void SpawnAdminMenu()
+        {
+            Section section = new Section(Section.GetSourceName(), Section.GetSourceName() + " Admin", "v1.0.0",
+                "Fooxiie", onlyAdmin: true);
+            
+            section.Insert();
+        }
+    }
+}
