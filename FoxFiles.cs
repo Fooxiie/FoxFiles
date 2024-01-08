5ba80eace78ce93245f0b65672a203524acafc54
Fox <dev.fooxiie@gmail.com>
2024-01-09 00:15:54 +0100

    Adding config feature (don't work for now)

--- FoxFiles.cs
+++ FoxFiles.cs
@@ -1,15 +1,74 @@
-﻿using Life;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using FoxFiles.Models;
+using Life;
+using FoxORM;
+using RTG;
+using UnityEngine;
 
 namespace FoxFiles
 {
-    public class FoxFiles: Plugin
+    public class FoxFiles : Plugin
     {
+        private FoxOrm _foxOrm;
+
         public FoxFiles(IGameAPI api) : base(api)
         {
-            UIManager.GetInstance().SpawnMenu();
-            UIManager.GetInstance().SpawnAdminMenu();
         }
-        
-        
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+            
+            try
+            {
+                UIManager.GetInstance().SpawnMenu();
+                UIManager.GetInstance().SpawnAdminMenu();
+
+                InitDatabase();
+            }
+            catch (Exception error)
+            {
+                Debug.LogError(error);
+            }
+        }
+
+        private void InitDatabase()
+        {
+            _foxOrm = new FoxOrm(Path.Combine(pluginsPath, "FoxFiles/database.sqlite"));
+
+            _foxOrm.RegisterTable<Configuration>();
+            VerifyConfigMigrations();
+        }
+
+        private async void VerifyConfigMigrations()
+        {
+            Dictionary<string, string> defaultConfig = new Dictionary<string, string>()
+            {
+                { "minAdminLvlRequired", "5" }
+            };
+            var listConfig = await _foxOrm.QueryAll<Configuration>();
+
+            foreach (var pair in defaultConfig)
+            {
+                var found = false;
+                foreach (var configuration in listConfig.Where(configuration => configuration.configCode == pair.Key))
+                {
+                    found = true;
+                }
+
+                if (found) continue;
+                var newConfig = new Configuration()
+                {
+                    value = pair.Value,
+                    configCode = pair.Key
+                };
+                var resultInsert = await _foxOrm.Save(newConfig);
+                Debug.LogWarning($"[Migration FoxFiles] adding config {pair.Key} : {resultInsert}");
+            }
+        }
     }
 }
---
+++ Models/Configuration.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace FoxFiles.Models
+{
+    public class Configuration
+    {
+        [PrimaryKey]
+        public string configCode { get; set; }
+
+        public string value { get; set; }
+    }
+}
